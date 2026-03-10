a924029765e46680a325e9911af55363302a8009
Stephanie Do <stephanie.do@lumentum.com>
2026-03-10 14:42:01 -0700

    Add (1) switch type, (2)bin5-bin10 cross threshold ratio (3) example of reduced port list for easy  port selection process

--- switch_config.py
+++ switch_config.py
@@ -1,3 +1,6 @@
-switch_ip_list = ["192.168.1.236","10.74.181.250"]
-switch_port_list = tuple(str(i) for i in range(1, 65))
-#switch_port_list = tuple(str(i) for i in [31, 32])
+switch_ip_list = ["192.168.1.55","10.74.181.250"]
+#switch_port_list = tuple(str(i) for i in range(1, 65))
+switch_port_list = tuple(str(i) for i in [21, 25])
+#switch_type = "NVIDIA"
+switch_type = "ARISTA"
+bin5_10_threshold_ratio = 0.1
